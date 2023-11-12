36734cbfdccbc274b3f06368afbea47d0316a9b7
DavidReinon <davidreinongarcia@gmail.com>
2023-11-12 23:47:45 +0100

    projecte acabat

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Screen1 from "./src/screens/Screen1";
@@ -13,13 +12,4 @@ const App = () => (
         </Tab.Navigator>
     </NavigationContainer>
 );
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
 export default App;
--- src/screens/Screen2.js
+++ src/screens/Screen2.js
@@ -1,94 +1,213 @@
-import React, { useState } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
-const ROWS = 10;
-const COLS = 10;
-const WORDS = ["software", "developer", "system", "app", "phone", "mobile"];
-
-const Screen2 = () => {
-    const [colors, setColors] = useState(
-        Array.from({ length: ROWS }, () =>
-            Array.from({ length: COLS }, () => "white")
-        )
-    );
-    const [letters, setLetters] = useState(
-        Array.from({ length: ROWS }, () =>
-            Array.from({ length: COLS }, () => "")
-        )
-    );
-    const [filled, setFilled] = useState(false);
-
-    const toggleColor = (row, col) => {
-        const newColors = [...colors];
-        newColors[row][col] =
-            newColors[row][col] === "white" ? "blue" : "white";
-        setColors(newColors);
+export default function Screen2() {
+    const initArray = () => {
+        return [...Array(10).keys()].map(() => {
+            return [...Array(10).keys()].map(() => {
+                return null;
+            });
+        });
+        // return Array.from({ length: 10 }, () =>
+        //     Array.from({ length: 10 }, () => null)
+        // );
     };
 
-    const addWord = (word, direction, row, col) => {
-        const newLetters = [...letters];
-        const wordLength = word.length;
+    const initColorArray = () => {
+        return [...Array(10).keys()].map(() => {
+            return [...Array(10).keys()].map(() => {
+                return "white";
+            });
+        });
+    };
 
-        if (direction === "vertical") { // ROWS - 1 COLS -1
-            if (row + wordLength <= ROWS - 1) {
-                for (let i = 0; i < wordLength; i++) {
-                    newLetters[row + i][col] = word[i];
-                }
+    const [grid, setGrid] = useState(initArray());
+    const [gridBackGroundColor, setGridBackGroundColor] = useState(
+        initColorArray()
+    );
+    const [rellenades, setRellenades] = useState(false);
+    const words = ["Phone", "Mobile", "Software", "Developer", "System", "App"];
+
+    const tryPutWord = (randomColumn, randomRow, wordIndex) => {
+        let letterIndex = 0;
+        let result = true;
+        let partialGrid = JSON.parse(JSON.stringify(grid)); // Mantén una copia de respaldo
+        let newGrid = [...grid];
+
+        while (letterIndex < words[wordIndex].length) {
+            if (
+                randomColumn >= 10 ||
+                randomRow >= 10 ||
+                grid[randomColumn] === undefined ||
+                grid[randomColumn][randomRow] !== null
+            ) {
+                result = false;
+                setGrid(partialGrid);
+                break;
             }
-        } else if (direction === "verticalInverted") { // 0
-            if (row - wordLength <= 0) {
-                for (let i = 0; i < wordLength; i++) {
-                    newLetters[row - 1][col] = word[i];
+
+            newGrid = newGrid.map((row, columnIndex) => {
+                if (columnIndex === randomColumn) {
+                    return row.map((cell, rowIndex) => {
+                        if (rowIndex === randomRow) {
+                            return words[wordIndex][letterIndex];
+                        }
+                        return cell;
+                    });
                 }
-            }
-        } else if (direction === "horizontal") { // ROWS - 1 COLS -1
-            if (col + wordLength <= COLS - 1) {
-                for (let i = 0; i < wordLength; i++) {
-                    newLetters[row][col + i] = word[i];
-                }
-            }
-        } else if (direction === "horizontalInverted") { // 0
-            if (col - wordLength <= 0) {
-                for (let i = 0; i < wordLength; i++) {
-                    newLetters[row][col - 1] = word[i];
-                }
-            }
-        } else if (direction === "diagonal") { // ROWS - 1 COLS -1
-            if (row + wordLength <= ROWS - 1 && col + wordLength <= COLS - 1) {
-                for (let i = 0; i < wordLength; i++) {
-                    newLetters[row + i][col + i] = word[i];
-                }
-            }
-        } else if (direction === "diagonalReverse") { // 0
-            if (row - wordLength <= 0 && col - wordLength <= 0) {
-                for (let i = 0; i < wordLength; i++) {
-                    newLetters[row - i][col - i] = word[i];
-                }
+                return row;
+            });
+            letterIndex += 1;
+
+            if (wordIndex === 0) {
+                // Phone, dirección diagonal '\'
+                randomColumn += 1;
+                randomRow += 1;
+            } else if (wordIndex === 1) {
+                // Mobile, dirección diagonal invertida '/'
+                randomColumn -= 1;
+                randomRow -= 1;
+            } else if (wordIndex === 2) {
+                // Software, direccion vertical normal
+                randomColumn += 1;
+            } else if (wordIndex === 3) {
+                // Developer, dirección vertical inversa
+                randomColumn -= 1;
+            } else if (wordIndex === 4) {
+                // System, dirección horizontal normal
+                randomRow += 1;
+            } else {
+                // App, dirección horizontal inversa
+                randomRow -= 1;
             }
         }
 
-        setLetters(newLetters);
+        if (result) {
+            setGrid(newGrid);
+            console.log(newGrid);
+        }
+
+        return result;
     };
 
-    const fillWords = () => {
-        addWord("software", "vertical", 0, 0);
-        addWord("developer", "verticalInverted", 7, 7);
-        addWord("system", "horizontal", 2, 1);
-        addWord("app", "horizontalInverted", 5, 0);
-        // addWord("phone", "diagonal", 0, 4);
-        // addWord("mobile", "diagonalReverse", 4, 7); // Cambio a diagonal inversa
-        setFilled(true);
+    const fillLettersInEmpty = () => {
+        const alfabeto = "abcdefghijklmnopqrstuvwxyz";
+
+        const nuevoGrid = grid.map((row) => {
+            return row.map((element) => {
+                if (element === null || element === "") {
+                    const indice = Math.floor(Math.random() * alfabeto.length);
+                    return alfabeto.charAt(indice);
+                } else {
+                    return element;
+                }
+            });
+        });
+        setRellenades(true);
+        setGrid(nuevoGrid);
+    };
+    const putWordsInRandomOrder = () => {
+        if (rellenades) return;
+        for (let i = 0; i < words.length; i++) {
+            let success = false;
+            let limitColumn = 10;
+            let complexMathRandom = "";
+
+            if (words[i] === "Software") {
+                // Palabras en dirección vertical
+                limitColumn = 10;
+                limitRow = 11 - words[i].length; // 11 por Math.Random // 0 a 2
+            } else if (words[i] === "Developer") {
+                // Palabras en dirección vertical invertido
+                complexMathRandom = "row";
+                limitColumn = 10;
+                limitRow = words[i].length - 1; // 8 a 9
+            } else if (words[i] === "System") {
+                // Palabras en dirección horizontal
+                limitColumn = 11 - words[i].length; // 11 por Math.Random // 0 a 4
+                limitRow = 10;
+            } else if (words[i] === "App") {
+                // Palabras en dirección horizontal invertido
+                complexMathRandom = "column";
+                limitColumn = words[i].length - 1; // 2 a 9
+                limitRow = 10;
+            } else if (words[i] === "Phone") {
+                // Palabra en dirección diagonal '\'
+                limitColumn = 11 - words[i].length; // 0 a 5
+                limitRow = 11 - words[i].length; // 0 a 5
+            } else if (words[i] === "Mobile") {
+                // Palabra en dirección diagonal invertida '/'
+                complexMathRandom = "columnRow";
+                limitColumn = words[i].length - 1; // 5 a 9
+                limitRow = words[i].length - 1; // 5 a 9
+            }
+
+            let attempts = 0;
+            const maxAttempts = 20; // Puedes ajustar este número según sea necesario
+
+            while (!success && attempts < maxAttempts) {
+                //const defaultGrid = [...grid];
+                let randomColumn;
+                let randomRow;
+
+                if (complexMathRandom === "columnRow") {
+                    randomColumn =
+                        Math.floor(Math.random() * (10 - limitColumn)) +
+                        limitColumn;
+                    randomRow =
+                        Math.floor(Math.random() * (10 - limitRow)) + limitRow;
+                } else if (complexMathRandom === "row") {
+                    randomColumn = Math.floor(Math.random() * limitColumn);
+                    randomRow =
+                        Math.floor(Math.random() * (10 - limitRow)) + limitRow;
+                } else if (complexMathRandom === "column") {
+                    randomColumn =
+                        Math.floor(Math.random() * (10 - limitColumn)) +
+                        limitColumn;
+                    randomRow = Math.floor(Math.random() * limitRow);
+                } else {
+                    randomColumn = Math.floor(Math.random() * limitColumn);
+                    randomRow = Math.floor(Math.random() * limitRow);
+                }
+
+                console.log(
+                    `Trying word ${words[i]} at (${randomColumn}, ${randomRow})`
+                );
+                if (tryPutWord(randomColumn, randomRow, i)) {
+                    success = true;
+                } else {
+                    attempts += 1;
+                }
+            }
+            if (!success) {
+                console.error(
+                    `Failed to place word ${words[i]} after ${maxAttempts} attempts. Skipping to the next word.`
+                );
+            }
+        }
+        fillLettersInEmpty();
+    };
+
+    const onPressTouchable = (indexRow, indexElement) => {
+        let newGridBackground = [...gridBackGroundColor];
+        newGridBackground[indexRow] = [...newGridBackground[indexRow]];
+        newGridBackground[indexRow][indexElement] =
+            newGridBackground[indexRow][indexElement] === "white"
+                ? "blue"
+                : "white";
+
+        setGridBackGroundColor(newGridBackground);
     };
 
     return (
         <View
             style={{
                 justifyContent: "center",
-                alignSelf: "center",
-                marginVertical: 80,
+                alignItems: "center",
+                marginVertical: 60,
             }}
         >
-            <TouchableOpacity onPress={() => !filled && fillWords()}>
+            <TouchableOpacity onPress={() => putWordsInRandomOrder()}>
                 <Text
                     style={{
                         fontSize: 40,
@@ -99,23 +218,38 @@ const Screen2 = () => {
                     Sopa de letras
                 </Text>
             </TouchableOpacity>
-            <View style={{ justifyContent: "center", alignItems: "center" }}>
-                {colors.map((row, rowIndex) => (
-                    <View key={rowIndex} style={{ flexDirection: "row" }}>
-                        {row.map((col, colIndex) => (
+
+            <View
+                style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                }}
+            >
+                {grid.map((row, indexRow) => (
+                    <View
+                        style={{
+                            flexDirection: "row",
+                        }}
+                        key={indexRow}
+                    >
+                        {row.map((element, indexElement) => (
                             <TouchableOpacity
-                                key={colIndex}
                                 style={{
-                                    backgroundColor: colors[rowIndex][colIndex],
-                                    width: 38,
+                                    width: 40,
+                                    backgroundColor:
+                                        gridBackGroundColor[indexRow][
+                                            indexElement
+                                        ],
                                     padding: 14,
                                     borderWidth: 1,
                                 }}
-                                onPress={() => toggleColor(rowIndex, colIndex)}
+                                key={indexElement}
+                                onPress={() =>
+                                    onPressTouchable(indexRow, indexElement)
+                                }
                             >
-                                <Text style={{ fontSize: 15 }}>
-                                    {letters[rowIndex][colIndex]}
-                                </Text>
+                                <Text style={{ fontSize: 13 }}>{element}</Text>
                             </TouchableOpacity>
                         ))}
                     </View>
@@ -123,6 +257,4 @@ const Screen2 = () => {
             </View>
         </View>
     );
-};
-
-export default Screen2;
+}
